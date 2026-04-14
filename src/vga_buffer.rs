e36e8cefa1c652db531fc94a9c07085bf16810b0
Dmytro Yatsenko <132939290+yatsenk@users.noreply.github.com>
2026-04-14 21:58:19 +0200

    Update vga_buffer.rs

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,15 @@
 use volatile::Volatile;
 use core::fmt;
+use lazy_static::lazy_static;
+
+// Useless since WRITER immutable
+lazy_static! {
+    pub static ref WRITER: Writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -91,7 +101,27 @@ impl Writer {
         Ok(())
     }
 
-    fn new_line(&mut self) {  }
+    fn new_line(&mut self) { 
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) { 
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
 pub fn println() {
