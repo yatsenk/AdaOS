3c0bfbd49ced7d1be4d569c99d3262151067ec36
Dmytro Yatsenko <132939290+yatsenk@users.noreply.github.com>
2026-04-18 15:51:00 +0200

    Create interrupts.rs

---
+++ src/interrupts.rs
@@ -0,0 +1,28 @@
+use crate::println;
+
+use x86_64::structures::idt::InterruptStackFrame;
+
+pub extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    let key = match scancode {
+        0x02 => Some('1'),
+        0x03 => Some('2'),
+        0x04 => Some('3'),
+        0x05 => Some('4'),
+        0x06 => Some('5'),
+        0x07 => Some('6'),
+        0x08 => Some('7'),
+        0x09 => Some('8'),
+        0x0a => Some('9'),
+        0x0b => Some('0'),
+        _ => None,
+    };
+
+    if let Some(k) = key {
+        println!("{}", k)
+    };
+}
