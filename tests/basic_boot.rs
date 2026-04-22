b7a5cb05ddd3d5890ceb8e86df8b340ee15614db
Dmytro Yatsenko <132939290+yatsenk@users.noreply.github.com>
2026-04-22 16:25:27 +0200

    Create basic_boot.rs

---
+++ tests/basic_boot.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(ada_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use ada_os::println;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    ada_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
