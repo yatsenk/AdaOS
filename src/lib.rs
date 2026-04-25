f598073aeac09c8fc403b04bf4d8c558e796c9f8
Dmytro Yatsenko <132939290+yatsenk@users.noreply.github.com>
2026-04-25 12:20:36 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,10 @@ pub mod gdt;
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize(); }
+    x86_64::instructions::interrupts::enable();
 }
+
 pub trait Testable {
     fn run(&self) -> ();
 }
