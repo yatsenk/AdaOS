0c7dfb98ac4882ec6c06c130b981ca9a9cd5e17b
Dmytro Yatsenko <132939290+yatsenk@users.noreply.github.com>
2026-05-01 20:20:45 +0200

    Added page fault handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,10 @@
+use crate::hlt_loop;
 use crate::println;
 use crate::print;
 use crate::gdt;
 
+
+use x86_64::structures::idt::PageFaultErrorCode;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
@@ -39,6 +42,7 @@ lazy_static! {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         };
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt[InterruptIndex::Timer.as_u8()]
             .set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_u8()]
@@ -55,11 +59,29 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT {:#?}", stack_frame);
 }
 
-extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame, 
+    _error_code: u64
+) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT {:#?}", stack_frame);
 }
 
-extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame, 
+    error_code: PageFaultErrorCode
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE_FAULT");
+    println!("Accessed Adress: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(
+    _stack_frame: InterruptStackFrame
+) {
     unsafe {
         PICS
             .lock()
