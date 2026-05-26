05eae5da4d6ce51e5558162a22b29604a2595a73
Dmytro Yatsenko <132939290+yatsenk@users.noreply.github.com>
2026-05-26 19:14:12 +0200

    Update memory.rs

--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,5 @@
 use x86_64::{
+    PhysAddr,
     VirtAddr, 
     structures::paging::PageTable,
 };
@@ -15,4 +16,38 @@ pub unsafe fn active_level_4_table(
     let page_table_ptr: *mut PageTable = virt_addr.as_mut_ptr();
 
     unsafe { &mut *page_table_ptr }
+}
+
+pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    translate_addr_inner(addr, physical_memory_offset)
+}
+
+fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    use x86_64::structures::paging::page_table::FrameError;
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_table, _) = Cr3::read();
+
+    let table_indexes = [
+        addr.p1_index(),
+        addr.p2_index(),
+        addr.p3_index(),
+        addr.p4_index(),
+    ];
+    let mut frame = level_4_table;
+
+    for &index in &table_indexes {
+        let virt = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr  = virt.as_ptr();
+        let table: &PageTable = unsafe { &*table_ptr };
+
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None, 
+            Err(FrameError::HugeFrame) => panic!("Huge pages are not supported"),
+        }
+    }
+
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
