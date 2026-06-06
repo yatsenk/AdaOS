f446b8f3cfb9f50a811e15d6d291fd8bcdc854a4
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-06-06 12:41:53 +0200

    + pub mod allocator;

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
 use core::panic::PanicInfo;
 
 #[cfg(test)]
@@ -15,6 +17,7 @@ pub mod serial;
 pub mod vga_buffer;
 pub mod gdt;
 pub mod memory;
+pub mod allocator;
 
 #[cfg(test)]
 entry_point!(test_kernel_main);
