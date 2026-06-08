fc726c73be4a51988297f6dea10a2505ecdd045d
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-06-08 21:18:25 +0200

    Locked Heap Allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,6 +1,48 @@
 use core::alloc::GlobalAlloc;
 use core::ptr::null_mut;
 
+use linked_list_allocator::LockedHeap;
+use x86_64::{
+    VirtAddr, structures::paging::{
+        FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB, mapper::MapToError
+    }
+};
+
+const HEAP_START: u64 = 0x_4242_4242_4242_8888;
+const HEAP_SIZE: u64 = 100 * 1024; // 100 KiB
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+pub fn init_heap(
+    mapper: &mut impl Mapper<Size4KiB>,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) -> Result<(), MapToError<Size4KiB>> {
+    let page_range = {
+        let heap_start = VirtAddr::new(HEAP_START as u64);
+        let heap_end = heap_start + HEAP_SIZE - 1u64;
+        let heap_start_page: Page<Size4KiB> = Page::containing_address(heap_start);
+        let heap_end_page = Page::containing_address(heap_end);
+        Page::range_inclusive(heap_start_page, heap_end_page)
+    };
+
+    for page in page_range {
+        let frame = frame_allocator
+            .allocate_frame()
+            .ok_or(MapToError::FrameAllocationFailed)?;
+
+        let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+
+        unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush(); }
+    }
+
+    unsafe {
+        ALLOCATOR.lock().init(HEAP_START as *mut u8, HEAP_SIZE as usize);
+    }
+
+    Ok(())
+}
+
 pub struct DummyAllocator;
 
 unsafe impl GlobalAlloc for DummyAllocator {
@@ -12,6 +54,3 @@ unsafe impl GlobalAlloc for DummyAllocator {
         panic!("Should never called.")
     }
 }
-
-#[global_allocator]
-static ALLOCATOR: DummyAllocator = DummyAllocator;
