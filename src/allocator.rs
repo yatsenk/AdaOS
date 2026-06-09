01521168db60210fe073bf11b6c95676f5d00fe1
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-06-09 17:47:38 +0200

    Changed private const HEAP_SIZE to public

--- src/allocator.rs
+++ src/allocator.rs
@@ -9,7 +9,7 @@ use x86_64::{
 };
 
 const HEAP_START: u64 = 0x_4444_4444_0000;
-const HEAP_SIZE: u64 = 100 * 1024; // 100 KiB
+pub const HEAP_SIZE: u64 = 100 * 1024; // 100 KiB
 
 #[global_allocator]
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
