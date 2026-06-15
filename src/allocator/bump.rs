1a87f9c2157876a86aa9da45e915d302d8831c73
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-06-15 21:14:34 +0200

    init method need to be unsafe; TODO: implement GlobalAlloc trait for BumpAllocator

--- src/allocator/bump.rs
+++ src/allocator/bump.rs
@@ -1,3 +1,5 @@
+use core::alloc::GlobalAlloc;
+
 pub struct BumpAllocator {
     heap_start: usize,
     heap_end: usize,
@@ -15,10 +17,20 @@ impl BumpAllocator {
         }
     }
 
-    fn init(&mut self, heap_start: usize, heap_size: usize) {
+    unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
         self.heap_start = heap_start;
         self.heap_end = heap_start + heap_size;
         self.next = heap_start;
+    }
+}
+
+unsafe impl GlobalAlloc for BumpAllocator {
+    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+        unimplemented!();   
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+        unimplemented!();
     }
 }
 
