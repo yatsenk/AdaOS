8a278e3cd172cd21d47a2dd223d0e6cbfbe72285
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-06-17 21:02:26 +0200

    implemented alloc and dealloc fn

--- src/allocator/bump.rs
+++ src/allocator/bump.rs
@@ -1,4 +1,7 @@
 use core::alloc::GlobalAlloc;
+use alloc::alloc::alloc;
+
+use super::{Locked, align_up};
 
 pub struct BumpAllocator {
     heap_start: usize,
@@ -24,13 +27,32 @@ impl BumpAllocator {
     }
 }
 
-unsafe impl GlobalAlloc for BumpAllocator {
+unsafe impl GlobalAlloc for Locked<BumpAllocator> {
     unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
-        unimplemented!();   
+        let mut bump = self.lock();
+
+        let alloc_start = align_up(bump.next, layout.align());
+        let alloc_end = match alloc_start.checked_add(layout.size()) {
+            Some(end) => end,
+            None => return core::ptr::null_mut(),
+        };
+
+        if alloc_end > bump.heap_end {
+            core::ptr::null_mut()
+        } else {
+            bump.next = alloc_start + layout.size();
+            bump.allocations += 1;
+            alloc_start as *mut u8
+        }
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
-        unimplemented!();
+        let mut bump = self.lock();
+
+        bump.allocations -= 1;
+        if bump.allocations == 0 {
+            bump.next = bump.heap_start;
+        }
     }
 }
 
